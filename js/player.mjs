77b81ca47cb8dc698908ade8a05441d741dd8466
belchushka <savvashu@gmail.com>
2022-04-07 18:44:59 +0300

    Audio Player instance

--- js/player.mjs
+++ js/player.mjs
@@ -5,6 +5,7 @@ export class AudioPlayer {
         this.audio = new Audio()
         this.audioPlaying = false
         this.playList = []
+        this.currentSongId = 0
         this.timeline = player.querySelector("#timeControl")
         this.volumeControll = player.querySelector("#volumeControl")
         this.songImage = player.querySelector("#songImage")
@@ -17,69 +18,87 @@ export class AudioPlayer {
         this.bind()
     }
 
-    bind(){
+    bind() {
         this.volumeControll.onchange = this.changeVolume.bind(this)
         this.timeline.onchange = this.setAudioTime.bind(this)
         this.playBtn.onclick = this.toggleAudio.bind(this)
-        this.audio.ontimeupdate = (ev)=>{
+        this.audio.ontimeupdate = (ev) => {
             this.currentTime = ev.target.currentTime
             this.timeline.value = this.currentTime
             this.setTotalTime(ev.target.duration)
             this.setStartTime(ev.target.currentTime)
         }
+        this.audio.onended = ()=>{
+            this.nextSong()
+        }
     }
 
 
-    playAudio(src, songImageUri){
+    playAudio(src, songImageUri) {
         clearInterval(this.playbackInterval)
         this.playList = src
-        this.songImage.setAttribute("src",songImageUri)
-        this.audio.src = this.playList[0]
-        this.timeline.setAttribute("max",Math.floor(this.audio.duration))
+        this.songImage.setAttribute("src", songImageUri)
+        this.currentSongId = 0
+        this.startAudio()
+    }
+
+    startAudio() {
+        this.audio.src = this.playList[this.currentSongId]
+        this.timeline.setAttribute("max", Math.floor(this.audio.duration))
         this.audio.volume = this.volume / 100
         this.audio.play()
         this.audioPlaying = true
         this.playBtn.classList.add("active")
     }
 
-    setStartTime(val){
+    nextSong() {
+        this.currentSongId + 1 < this.playList.length ? this.currentSongId += 1 : this.currentSongId += 0
+        this.startAudio()
+    }
+
+    prevSong() {
+        this.currentSongId - 1 > 0 ? this.currentSongId -= 1 : this.currentSongId -= 0
+        this.startAudio()
+    }
+
+    setStartTime(val) {
         this.currentTime = val
         const minutes = Math.floor(this.currentTime / 60)
-        let  seconds = Math.floor(this.currentTime % 60)
+        let seconds = Math.floor(this.currentTime % 60)
         seconds = String(seconds).length < 2 ? "0" + String(seconds) : seconds
-        this.startTimeIndicator.textContent = minutes + ":"+seconds
+        this.startTimeIndicator.textContent = minutes + ":" + seconds
     }
 
-    setTotalTime(val){
+    setTotalTime(val) {
         this.duration = val
         const minutes = Math.floor(this.duration / 60)
-        let  seconds = Math.floor(this.duration % 60)
+        let seconds = Math.floor(this.duration % 60)
         seconds = String(seconds).length < 2 ? "0" + String(seconds) : seconds
-        this.totalTimeIndicator.textContent = minutes + ":"+seconds
+        this.totalTimeIndicator.textContent = minutes + ":" + seconds
     }
 
-    toggleAudio(){
+    toggleAudio() {
         this.playBtn.classList.toggle("active")
-        if (this.audioPlaying){
+        if (this.audioPlaying) {
             this.audio.pause()
             this.audioPlaying = false
-        }else{
+        } else {
             this.audio.play()
             this.audioPlaying = true
         }
     }
 
-    pauseAudio(){
+    pauseAudio() {
         this.audio.pause()
         this.audioPlaying = false
         this.playBtn.classList.remove("active")
     }
 
-    destroyAudio(){
-        this.audio=null
+    destroyAudio() {
+        this.audio = null
     }
 
-    changeVolume(ev){
+    changeVolume(ev) {
         this.volume = ev.target.value
         this.audio.volume = this.volume / 100
     }
